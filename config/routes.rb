b95c9f656f0f822c0a4003171e8f4b5ced44fa11
Lisa Li <lisa.li72159@gmail.com>
2019-11-18 18:01:59 -0500

    Updated backend for follows

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :update, :show] 
+    resources :users, only: [:create, :update, :show]
     resource :session, only: [:create, :destroy]
     resources :boards, only: [:create, :update, :index, :destroy, :show]
     resources :pins, only: [:create, :update, :index, :destroy, :show]
     resources :follows, only: [:index, :create, :destroy]
+    
   end
 
   root "static_pages#root"
